dfa50e6e08d044331c12ba1920b4c327d5924923
heypigg <erikmolnar@gmail.com>
2024-09-24 23:44:33 -0400

    Add files to terraform

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,4 +1,52 @@
 # Create an S3 bucket
 resource "aws_s3_bucket" "oncall_bucket" {
   bucket = "oncall-website"
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [aws_s3_bucket_ownership_controls.example]
+
+  bucket = aws_s3_bucket.example.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "example" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    id = "rule-1"
+
+    # ... other transition/expiration actions ...
+
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = aws_s3_bucket.example.id
+  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+}
+
+data "aws_iam_policy_document" "allow_access_from_another_account" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["123456789012"]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.example.arn,
+      "${aws_s3_bucket.example.arn}/*",
+    ]
+  }
+}
+
