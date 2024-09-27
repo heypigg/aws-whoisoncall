9961fb5a9f74b718d6c40a8229bc86951f1fda3c
heypigg <erikmolnar@gmail.com>
2024-09-27 00:02:43 -0400

    add ecr

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,24 +1,64 @@
-resource "aws_ecs_cluster" "foo" {
+resource "aws_ecs_cluster" "oncallwebsite" {
   name = "go-mets"
-
-  setting {
-    name  = "containerInsights"
-    value = "enabled"
-  }
 }
 
-resource "aws_ecs_cluster" "test" {
-  name = "example"
+# resource "aws_ecs_cluster" "test" {
+#   name = "example"
 
-  configuration {
-    execute_command_configuration {
-      #kms_key_id = aws_kms_key.example.arn
-      logging    = "OVERRIDE"
+#   configuration {
+#     execute_command_configuration {
+#       #kms_key_id = aws_kms_key.example.arn
+#       logging    = "OVERRIDE"
 
-    #   log_configuration {
-    #     cloud_watch_encryption_enabled = true
-    #     cloud_watch_log_group_name     = aws_cloudwatch_log_group.example.name
-    #   }
+#     #   log_configuration {
+#     #     cloud_watch_encryption_enabled = true
+#     #     cloud_watch_log_group_name     = aws_cloudwatch_log_group.example.name
+#     #   }
+#     }
+#   }
+# }
+
+
+resource "aws_ecs_task_definition" "my_task" {
+  family                   = "oncallwebsite"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"  # Adjust as needed
+  memory                   = "512"  # Adjust as needed
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "server-app"
+      image     = "${var.account_number}.dkr.ecr.us-east-1.amazonaws.com/oncallwebsite:latest"
+      cpu       = 256
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 3001
+          hostPort      = 3001
+          protocol      = "tcp"
+        }
+      ]
     }
+  ])
+}
+
+## Build Container Registry
+resource "aws_ecr_repository" "oncallwebsite_ecr_repo" {
+  name = "oncallwebsite-repo" # Name of the ECR repository
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  # encryption_configuration {
+  #   encryption_type = "AES256" # Default encryption, or use "KMS" for using AWS KMS keys
+  # }
+
+  tags = {
+    Environment = "dev"
+    Project     = "oncallwebsite"
   }
 }
